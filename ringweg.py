ab7c008ff894369a2718fd93aa9822361d5aa121
Tom <tom.oldenbeuving@gmail.com>
2020-03-11 08:48:17 +0100

    11/2/2020 8:50

--- ringweg.py
+++ ringweg.py
@@ -1,6 +1,7 @@
 from microbit import *
 import time
 
+A1w_sens = display.read_light_level()
 # voor het voorbeeld zijn de volgende inputs uitgeschakeld
 #vw_sens = sensorteller()
 #ls_sens = display.read_light_level()
@@ -11,34 +12,35 @@ import time
 #rk_sens = display.read_light_level()
 #A28z_sens = display.read_light_level()
 #A28n_sens = display.read_light_level()
-A1w_sens = display.read_light_level()
 #A1O_sens = display.read_light_level()
 
-def sens(sensorlocal):
+def sens(sensorlocal): #dit is de teller die 1 optelt bij een variable als het te donker wordt(als er dus een atuo voor langs rijdt)
     while True:   
         t_end = time.time()
         seconds = 10
         afslag = 0 
-        while time.time() < t_end + seconds:
+        while time.time() < t_end + seconds: #dit is de klok waardoor die maar voor 1 minuut autos telt
             time.sleep(.1)
             if sensorlocal < 120:
                 afslag += 1
             if time.time == t_end: 
-                return afslag
+                return afslag #aan het eind van de loop stuurt de functie (da's altijd een 'def') de variable afslag terug naar de code
 
-van_wijkstraat = sens(vw_sens)
-leusderweg = sens(ls_sens)
-utrechtse_weg = sens(ut_sens)
-vermeerstraat = sens(vs_sens)
-arhnemseweg = sens(aw_sens)
-ringwegranderbroek = sens(rb_sens)
-ringwegkruiskamp = sens(rk_sens)
-A28z = sens(A28z_sens)
-A28n = sens(A28n_sens)
 A1w = sens(A1w_sens)
-A1O = sens(A1w_sens)
+#van_wijkstraat = sens(vw_sens)
+#leusderweg = sens(ls_sens)
+#utrechtse_weg = sens(ut_sens)
+#vermeerstraat = sens(vs_sens)
+#arhnemseweg = sens(aw_sens)
+#ringwegranderbroek = sens(rb_sens)
+#ringwegkruiskamp = sens(rk_sens)
+#A28z = sens(A28z_sens)
+#A28n = sens(A28n_sens)
+#A1O = sens(A10_sens)
 
-if A1w > 20:
+
+#hier wordt de 'afslag vergeleken met een getal om te bepalen waar de automobilist heen moet
+if A1w > 20: 
     display.scroll("via Energieweg, Amersfoortse Straat")
 elif A1w > 5 : 
    display.scroll("via Outputweg, Rondweg Ost, Bergpas") 
